20f3bf84c389479f2d35a106b0447256a6552760
MA-Zarei <maza7728@gmail.com>
2025-07-30 16:41:12 +0330

    17- Fix Custom 404.html
    Fix for GitHub Pages routing with 404 redirect.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const redirectParam = window.location.search;
+if (redirectParam) {
+  const realPath = decodeURIComponent(redirectParam.slice(1));
+  window.history.replaceState(null, '', realPath);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
